1dcd3a6e7f5b57d0a7c7c85ca010ed1a908c3611
Ufuk K <ufukk@hotmail.com>
2020-01-31 15:29:42 +0300

    Add project files.

---
+++ SimpleFab/SimpleFab/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleFab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void OnFabMenuClick(object sender, EventArgs args)
+        {
+            Application.Current.MainPage.DisplayAlert("Dialog", "Hello", "OK");
+
+        }
+    }
+}
